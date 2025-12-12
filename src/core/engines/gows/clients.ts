f8bcaebf4a245f9349a38f2c6ffc9a68ebbe6eb0
devlikepro <waha@devlike.pro>
2025-12-12 12:15:31 +0700

    [core] GOWS - make grpc pool for clients
    fix #1715

--- src/core/engines/gows/clients.ts
+++ src/core/engines/gows/clients.ts
@@ -1,15 +1,25 @@
 import * as grpc from '@grpc/grpc-js';
 import { messages } from '@waha/core/engines/gows/grpc/gows';
+import { Pool, SizedPool } from '@waha/core/engines/gows/pools';
+
+let CLIENTS: Pool<messages.MessageServiceClient> | null = null;
+let STREAM_CLIENTS: Pool<messages.EventStreamClient> | null = null;
 
 export const GetMessageServiceClient = (
   session: string,
   address: string,
   credentials: grpc.ChannelCredentials,
 ): messages.MessageServiceClient => {
-  return new messages.MessageServiceClient(address, credentials, {
-    'grpc.max_send_message_length': 128 * 1024 * 1024,
-    'grpc.max_receive_message_length': 128 * 1024 * 1024,
-  });
+  if (!CLIENTS) {
+    const factory = () => {
+      return new messages.MessageServiceClient(address, credentials, {
+        'grpc.max_send_message_length': 128 * 1024 * 1024,
+        'grpc.max_receive_message_length': 128 * 1024 * 1024,
+      });
+    };
+    CLIENTS = new SizedPool(16, factory);
+  }
+  return CLIENTS.get(session);
 };
 
 export const GetEventStreamClient = (
@@ -17,8 +27,14 @@ export const GetEventStreamClient = (
   address: string,
   credentials: grpc.ChannelCredentials,
 ): messages.EventStreamClient => {
-  return new messages.EventStreamClient(address, credentials, {
-    'grpc.max_send_message_length': 128 * 1024 * 1024,
-    'grpc.max_receive_message_length': 128 * 1024 * 1024,
-  });
+  if (!STREAM_CLIENTS) {
+    const factory = () => {
+      return new messages.EventStreamClient(address, credentials, {
+        'grpc.max_send_message_length': 128 * 1024 * 1024,
+        'grpc.max_receive_message_length': 128 * 1024 * 1024,
+      });
+    };
+    STREAM_CLIENTS = new SizedPool(16, factory);
+  }
+  return STREAM_CLIENTS.get(session);
 };
--- src/core/engines/gows/session.gows.core.ts
+++ src/core/engines/gows/session.gows.core.ts
@@ -765,7 +765,6 @@ export class WhatsappSessionGoWSCore extends WhatsappSession {
     this.status = WAHASessionStatus.STOPPED;
     this.events?.stop();
     this.stopEvents();
-    this.client?.close();
     this.mediaManager.close();
   }
 
